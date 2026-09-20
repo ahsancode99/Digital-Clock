d97e65f7da9d7f38c7063fb347aa6333b88e5719
ahsancode99 <ahsanff922@gmail.com>
2026-09-20 17:58:30 +0500

    Correct day display for zero day value
    Fix day display for the case when day is 0.

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ function digitalClock (){
     date_span.innerText = date;
     month_span.innerText = months_arr[month];
     day_span.innerText = days_arr[day - 1]
+    if( day == 0){
+        day_span.innerText = days_arr[6]
+    }
 
 
     // seconds 0 fix         ////////////
